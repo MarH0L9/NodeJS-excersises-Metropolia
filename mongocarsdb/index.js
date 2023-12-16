d15b3f6af0eaebfb93a93271cfced4a55133b43c
Marco <143179277+MarH0L9@users.noreply.github.com>
2023-12-16 17:41:42 +0200

    Create index.js

---
+++ mongocarsdb/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+app.use(express.json());
+app.use('/', routes);
+
+//MongoDB connection
+const mongoURL = 'mongodb+srv://marcoviope:TZvcPICt7oOXwVID@cluster0.qpclkuu.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { dbName: 'cardb', useNewUrlParser: true , useUnifiedTopology: true});         
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
